dcdb245ceccfd3a61b470fc71f6ae8f51d933cbd
Alex <you@example.com>
2024-03-28 12:26:24 +0100

    1

--- src/main/java/com/project/CheapCheese/SecurityConfig.java
+++ src/main/java/com/project/CheapCheese/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.project.CheapCheese.models;
+package com.project.CheapCheese;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
